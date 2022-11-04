248eea351fd1d3ad404dfd67bd5d941ba38a72db
Coleman Escue <99232536+Nam3loc@users.noreply.github.com>
2022-11-04 18:18:13 -0400

    Mostly working, fix darts and collision

--- js/pseudocode.js
+++ js/pseudocode.js
@@ -1,9 +1,21 @@
 /*
+// JS Game Dev video
+https://www.youtube.com/watch?v=GFO_txvwK_c&t=11919s
+
 // Path following video
 https://www.youtube.com/watch?v=rlZYT-uvmGQ
 
 // Bloons Sprite Sheets
 https://www.spriters-resource.com/pc_computer/bloonstowerdefense6/
+
+// How to draw a path (9:00 on how to curve the path)
+https://www.youtube.com/watch?v=HUp8yDpfFm4
+
+// Moving an image on canvas
+https://www.youtube.com/watch?v=6dpxrBTESvQ
+
+// Tower Defense Tutorial
+https://www.youtube.com/watch?v=C4_iRLlPNFc
 
 // Grid
 My idea is to get a grid on the DOM and to traverse the grid similar to chess notation. To somehow give each grid a value and to send the balloons down the valued grid points in the form of a path. Instead of trying to somehow create a path and then a path following algorithm.
@@ -52,4 +64,295 @@ I want the monkey to be able to throw a dart. Probably just stick to dart monkey
 -Giving gold per second of gameplay
 
 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// Creating Bloon Animation
+const redBloonImg = document.querySelector('#red-bloon-img');
+const redBloonCtx = redBloonImg.getContext('2d');
+const redBloonWidth = redBloonImg.width = 50;
+const redBloonHeight = redBloonImg.height = 50;
+
+
+
+// Animated red balloon
+const redBalloonImage = new Image();
+redBalloonImage.src = './images/red-bloon-sprite-removebg.png';
+const secondSpriteWidth = 115;
+const secondSpriteHeight = 115;
+
+function redBloonAnimate(){
+    redBloonCtx.clearRect(0, 0, redBloonWidth, redBloonHeight);
+    redBloonCtx.drawImage(redBalloonImage, 55, 55, secondSpriteWidth, secondSpriteHeight, 0, 0, redBloonWidth, redBloonHeight);
+   
+    requestAnimationFrame(redBloonAnimate);
+}
+redBloonAnimate();
+*/
+
+
+
+/*
+// Drawing path
+let path;
+
+ctx.beginPath(); // Resets the current default path
+
+ctx.stroke(); // Strokes the current default path
+
+// First line of the path
+ctx.moveTo(0, 500); // Creates a new subpath with the given point
+ctx.lineTo(1000, 500); //x = 250, y = 50 is a straight line... Creates a new line along the current subpath
+
+// Second (bottom) line of the path
+ctx.moveTo(0, 550);
+ctx.lineTo(1000, 550);
+// ctx.fill('brown')
+
+// ctx.closePath() // Closes a path if there is an adjacent intersect
+
+// ctx.fill() // Fills a closed path (shape)
+
+
+
+
+
+
+
+
+// Score counter?
+// if(this.x < 0 - this.width){
+//     health--;
+//     if(score <= 0){
+//         gameOver = true;   
+//     }
+// } 
+
+
+
+
+
+
+
+
+
+
+// Balloon Function Breakdown for game function build
+
+const redBalloonImage = new Image();
+redBalloonImage.src = './images/red-bloon-sprite-removebg.png'
+redBalloonImage.style.border = '5px solid black';
+let balloonWidth = redBalloonImage.width = 220;
+let balloonHeight = redBalloonImage.height = 220;
+
+redBalloonImage = new Image();
+redBalloonImage.src = './images/red-bloon-sprite-removebg.png'
+balloonWidth = this.redBalloonImage.width = 220;
+balloonHeight = this.redBalloonImage.height = 220;
+
+
+function draw() {
+    ctx.globalCompositeOperation = 'destination-under';
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    // ctx.strokeRect(x, 500, canvasWidth/20, canvasHeight/20);
+    ctx.fillStyle = 'tan';
+    ctx.fillRect(0, 500, 1000, 80);
+
+    ctx.drawImage(redBalloonImage, 0, 0, balloonWidth, balloonHeight, x, 500, canvasHeight/10, canvasHeight/10);
+
+    requestAnimationFrame(draw);
+    drawScore();
+    drawTimer();
+    drawGold();
+    x++;
+    if(x > canvasWidth){
+        x = 0;
+        health--;
+    }
+}
+draw();
+
+
+
+
+
+    // [...balloon].forEach(object => {
+    //             console.log(balloon.length)
+    //             object.draw()
+    //         });
+
+
+
+
+
+// THIS IS THE WORKING BALLOON ========================================================
+
+// const redBalloonImage = new Image();
+// redBalloonImage.src = './images/red-bloon-sprite-removebg.png'
+// redBalloonImage.style.border = '5px solid black';
+// let balloonWidth = redBalloonImage.width = 220;
+// let balloonHeight = redBalloonImage.height = 220;
+// let x = 0;
+
+// function draw() {
+//     drawScore();
+//     ctx.globalCompositeOperation = 'destination-under';
+//     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+//     // ctx.strokeRect(x, 500, canvasWidth/20, canvasHeight/20);
+//     ctx.fillStyle = 'tan';
+//     ctx.fillRect(0, 500, 1000, 80);
+
+//     ctx.drawImage(redBalloonImage, 0, 0, balloonWidth, balloonHeight, x, 500, canvasHeight/10, canvasHeight/10);
+
+//     requestAnimationFrame(draw);
+    
+//     x++;
+//     if(x > canvasWidth){
+//         x = 0;
+//     }
+// }
+// draw();
+
+// THIS IS THE WORKING BALLOON ========================================================
+
+
+
+
+
+
+
+
+
+
+
+
+// THIS IS THE WORKING ANIMATED MONKEY====================================================================
+// Creating Monkey Variables
+const monkeyImg = document.querySelector('#monkeyImg');
+const monkeyImgCtx = monkeyImg.getContext('2d');
+const monkeyImgWidth = monkeyImg.width = 80;
+const monkeyImgHeight = monkeyImg.height = 80;
+
+
+
+
+// Defining Monkey Image
+const picOfMonkey = new Image();
+picOfMonkey.src = './images/dart-monkey-sprite.png'
+const spriteWidth = 218;
+const spriteHeight = 218;
+let frameX = 0;
+let gameFrame = 0;
+const staggerFrames = 40;
+let towerX = 0;
+let towerY = 0;
+
+
+
+
+// Drawing monkey img
+function monkeyAnimate() {
+    monkeyImgCtx.clearRect(0, 0, monkeyImgWidth, monkeyImgHeight);
+    monkeyImgCtx.drawImage(picOfMonkey, frameX * spriteWidth, 65, spriteWidth, spriteHeight, towerX, towerY, monkeyImgWidth, monkeyImgHeight);
+    if(gameFrame % staggerFrames == 0){
+        if(frameX < 4) frameX++;
+        else frameX = 0;
+    }
+
+    gameFrame++;
+    requestAnimationFrame(monkeyAnimate);
+}
+monkeyAnimate();
+// THIS IS THE WORKING ANIMATED MONKEY====================================================================
+
+
+
+
+
+
+
+
+
+
+// Making the balloon draggable
+// let draggable = false;
+// let currentX = canvasWidth/2;
+// let currentY = canvasHeight/2;
+
+// Creating canvas mouse events
+// canvas.onmousedown = (evt) => {
+//     console.log('mousedown', evt.layerX, evt.layerY)
+//     if(evt.layerX <= (currentX + redBalloonImage.width/2) && evt.layerX >= (currentX - redBalloonImage.width/2) && (evt.layerY <= (currentY + redBalloonImage.height/2) && evt.layerX >= (currentY - redBalloonImage.height/2))) {
+//         draggable = true;
+//         console.log('clicked image');
+//     } else {
+//         console.log("didn't click the image");
+//     }
+// }
+
+// canvas.onmouseup = (evt) => {
+//     console.log('mouseup', evt.layerX, evt.layerY)
+// }
+
+// canvas.onmouseout = (evt) => {
+//     console.log('mouseout', evt.layerX, evt.layerY)
+// }
+
+
+
+
+
+
+
+// let balloon = [];
+// class Balloon {
+//     constructor() {
+//         this.redBalloonImage = new Image();
+//         this.redBalloonImage.src = './images/red-bloon-sprite-removebg.png'
+
+//         this.balloonWidth = this.redBalloonImage.width = 220;
+//         this.balloonHeight = this.redBalloonImage.height = 220;
+//         this.x = 0;
+//         this.y = 500;
+//     }
+
+//     draw() {
+//         ctx.drawImage(this.redBalloonImage, 0, 0, this.balloonWidth, this.balloonHeight, this.x, this.y, canvasHeight/10, canvasHeight/10);
+//         this.x++;
+
+//         if(this.x > canvasWidth){
+//             this.x = 0;
+//             health--;
+//         }
+//     }
+// }
+
+
+// let redBalloon = new Balloon();
+// let timeToNextBalloon = 0;
+// let balloonInterval = 20;
+
+
+    // redBalloon.draw();
+
+    // timeToNextBalloon++;
+    // if(timeToNextBalloon > balloonInterval){
+    //     balloons.push(new Balloon());
+    //     timeToNextBalloon = 0;
+    // }
+    // [...balloons].forEach(object => object.draw());
 */
--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@
 
 const body = document.querySelector('body');
 
-const createGrid = document.querySelector('#game-board');
-
+const createGameBoard = document.querySelector('#game-board');
 
 
 
@@ -16,82 +15,344 @@ const canvasHeight = canvas.height = 1000;
 
 
 
-// Creating Monkey Animation
-const monkeyImg = document.querySelector('#monkeyImg');
-const monkeyImgCtx = monkeyImg.getContext('2d');
-const monkeyImgWidth = monkeyImg.width = 80;
-const monkeyImgHeight = monkeyImg.height = 80;
+// Creating path
+
+function drawPath() {
+    ctx.fillStyle = 'tan';
+    ctx.fillRect(0, 500, 1000, 80);
+}
 
 
 
 
 // Classes
 
+let balloons = [];
 class Balloon {
-    constructor(color, layers, speed) {
-        this.color = color;
-        this.layers = layers;
-        this.speed = speed;
+    constructor() {
+        this.redBalloonImage = new Image();
+        this.redBalloonImage.src = './images/red-bloon-sprite-removebg.png';
+
+        this.balloonWidth = this.redBalloonImage.width = 220;
+        this.balloonHeight = this.redBalloonImage.height = 220;
+        this.x = -100;
+        this.y = 500;
+        this.speed = 0.6;
+        this.movement = this.speed;
+    }
+
+    update() {
+        this.x += this.movement;
+    }
+
+    draw() {
+        ctx.drawImage(this.redBalloonImage, 0, 0, this.balloonWidth, this.balloonHeight, this.x, this.y, canvasHeight/10, canvasHeight/10);
     }
 }
+
+function handleBalloons() {
+    for(let i = 0; i < balloons.length; i++) {
+
+        if (balloons[i].x > canvasWidth){
+            health--;
+            balloons.splice(i, 1);
+        }
+
+        balloons[i].update();
+        balloons[i].draw();
+    }
+    if (frame % 100 === 0) {
+        balloons.push(new Balloon())
+    }
+}
+
+
+
+// Projectiles
+const projectiles = [];
+
+class Projectile {
+    constructor(x, y) {
+        this.dartImage = new Image();
+        this.dartImage.src = 'images/dart-sprite-removebg.png'        
+
+        this.x = x; 
+        this.y = y;
+        this.width = 20;
+        this.height = 20;
+        this.speed = 0.6;
+    }
+
+    update() {
+        this.x += this.speed;
+    }
+
+    draw() {
+        ctx.drawImage(this.dartImage, 0, 0, this.dartImage.width, this.dartImage.height, this.x, this.y, this.width, this.height);
+    }
+}
+
+function handleProjectiles() {
+    for (let i = 0; i < projectiles.length; i++) {
+        projectiles[i].update();
+        projectiles[i].draw();
+
+        if(projectiles[i] && projectiles[i].x > canvas.width - cellSize){
+            projectiles.splice(i, 1);
+            i--;
+        }
+    }
+}
+
+
+
+
+// Creating monkey towers
+const monkeys = [];
 
 class Monkey {
-    constructor() {
-        
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+        this.shooting = false;
+        this.projectiles = [];
+        this.timer = 0;
+    }
+
+    draw() {
+        this.monkeyImage = new Image();
+        this.monkeyImage.src = 'images/Dart-Monkey-removebg.png' 
+        ctx.drawImage(this.monkeyImage, 0, 0, this.monkeyImage.width, this.monkeyImage.height, this.x, this.y, this.width, this.height);
+
+        // ctx.fillStyle = 'blue';
+        // ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    update() {
+        this.timer++;
+        if (this.timer %100 === 0) {
+            projectiles.push(new Projectile(this.x + 50, this.y + 50));
+        }
+    }
+}
+
+canvas.addEventListener('click', function(){
+    const gridPositionX = mouse.x - (mouse.x % cellSize);
+    const gridPositionY = mouse.y - (mouse.y % cellSize);
+    for(let i = 0; i < monkeys.length; i++) {
+        if(monkeys[i].x === gridPositionX && monkeys[i]. y === gridPositionY){
+            return;
+        }
+    }
+
+    let monkeyCost = 100;
+    if (gold >= monkeyCost) {
+        monkeys.push(new Monkey(gridPositionX, gridPositionY));
+        gold -= monkeyCost;
+    }
+})
+
+function handleMonkeys() {
+    for(let i = 0; i < monkeys.length; i++){
+        monkeys[i].draw();
+        monkeys[i].update();
     }
 }
 
 
 
-// Declare variable
+// Mouse
+const mouse = {
+    x: undefined,
+    y: undefined,
+    width: 0.1,
+    height: 0.1
+}
 
-let redBalloon = new Balloon('red', 1, 0.5);
+let canvasPosition = canvas.getBoundingClientRect();
+canvas.addEventListener('mousemove', function(evt){
+    mouse.x = evt.x - canvasPosition.left;
+    mouse.y = evt.y - canvasPosition.top;
+});
+canvas.addEventListener('mouseleave', function() {
+    mouse.y = undefined;
+    mouse.y = undefined;
+})
 
 
 
+// Creating gameGrid
+const cellSize = 100;
+const cellGap = 3;
+const gameGrid = [];
 
-// Defining Monkey Image
-const picOfMonkey = new Image();
-picOfMonkey.src = './images/dart-monkey-sprite.png'
-const spriteWidth = 218;
-const spriteHeight = 218;
-let frameX = 0;
-let gameFrame = 0;
-const staggerFrames = 40;
-
-
-// Drawing monkey img
-function monkeyAnimate() {
-    monkeyImgCtx.clearRect(0, 0, monkeyImgWidth, monkeyImgHeight);
-    monkeyImgCtx.drawImage(picOfMonkey, frameX * spriteWidth, 65, spriteWidth, spriteHeight, 0, 0, monkeyImgWidth, monkeyImgHeight);
-    if(gameFrame % staggerFrames == 0){
-        if(frameX < 4) frameX++;
-        else frameX = 0;
+class Cell {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
     }
 
-    gameFrame++;
-    requestAnimationFrame(monkeyAnimate);
+    draw() {
+        if (mouse.x && mouse.y && collision(this, mouse)) {
+            ctx.strokeStyle = 'black';
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
+        }
+    }
 }
-monkeyAnimate();
+
+function createGrid() {
+    for(let y = cellSize; y < canvas.height; y += cellSize){
+        for(let x = 0; x < canvas.width; x += cellSize){
+            gameGrid.push(new Cell(x, y));
+        }
+    }
+}
+createGrid();
+
+function handleGameGrid() {
+    for(let i = 0; i < gameGrid.length; i++) {
+        gameGrid[i].draw();
+    }
+}
 
 
 
-// Drawing path
-let path;
 
-ctx.beginPath(); // Resets the current default path
+// function game() {
+//     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+// }
 
-// First line of the path
-ctx.moveTo(0, 500); // Creates a new subpath with the given point
-ctx.lineTo(1000, 500); //x = 250, y = 50 is a straight line... Creates a new line along the current subpath
 
-// Second (bottom) line of the path
-ctx.moveTo(0, 550);
-ctx.lineTo(1000, 550);
-// ctx.fill('brown')
 
-// ctx.closePath() // Closes a path if there is an adjacent intersect
+// Tower Vision Radius
+// let getBalloonCoordinates = new Balloon();
+let a = 0;
+let b = 0;
+// let Tx = towerX; // Tower x
+// let Ty = towerY; // Tower y
+// let Ex = balloon[0].x; // Enemy x
+// let Ey = 500; // Enemy y
+let rx = 0; // Tower vision radius x
+let ry = 0; // Tower vision radius y
 
-// ctx.fill() // Fills a closed path (shape)
+// Given tower position (Tx, Ty) and enemy position (x, y). And tower vision radius (rx and ry)
 
-ctx.stroke(); // Strokes the current default path
+function towerVision() {
+    a = (Ex - Tx);
+    a = a * a / (rx * rx)
+    b = (Ey - Ty);
+    b = b * b / (ry * ry)
+
+    enemyInRange = a + b <= 1;
+
+    // Drawing vision radius
+
+    ctx.arc(Tx, Ty, (rx, ry), 0, Math.PI * 2)
+    ctx.fill();
+    return;
+}
+// towerVision();
+
+
+
+
+// Variable definitions for score
+let health = 100;
+let gold = 300;
+let gameOver = false;
+ctx.font = '35px serif';
+
+
+
+function drawScore() {
+    ctx.fillStyle = 'white';
+    ctx.fillText("Health: " + health, 55, 80);
+}
+
+function createTimer() {
+    // const minutes = document.querySelector('#minutes');
+    // const seconds = document.querySelector('#seconds');
+    let minutes = '00';
+    let seconds = '00';
+
+    let totalSeconds = 0;
+    setInterval(setTime, 1000);
+
+    function setTime() {
+        totalSeconds++;
+        seconds = pad(totalSeconds % 60);
+        minutes = pad(parseInt(totalSeconds / 60));
+    }
+
+    function pad(val) {
+        let valStr = val + "";
+        if(valStr.length < 2) {
+            return "0" + valStr;
+        } else {
+            return valStr;
+        }
+    }
+
+    return (`${minutes}:${seconds}`);
+}
+
+function drawTimer() {
+    ctx.fillStyle = 'white';
+    ctx.fillText("Timer: " + createTimer(), 375, 80);
+}
+
+function drawGold() {
+    ctx.fillStyle = 'white';
+    ctx.fillText("Gold: " + gold, 775, 80);
+}
+
+function drawGameOver() {
+    ctx.textAlign = 'center';
+    ctx.fillStyle = 'white';
+    ctx.fillText("GAME OVER", canvas.width/2, canvas.height/2 + 5);
+}
+
+
+
+
+
+// Game Function
+
+let frame = 0;
+
+function game() {
+    ctx.globalCompositeOperation = 'destination-under';
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    drawPath();
+
+    handleGameGrid();
+    handleMonkeys();
+    handleProjectiles();
+    handleBalloons();
+
+    drawScore();
+    drawTimer();
+    drawGold();
+
+    frame++;
+
+    if (health <= 0){
+        drawGameOver();
+    } else {
+        requestAnimationFrame(game);
+    }
+}
+game();
+
+function collision(first, second) {
+    if(!(first.x > second.x + second.width || 
+        first.x + first.width < second.x || 
+        first.y > second.y + second.height || 
+        first.y + first.height < second.y)) {
+            return true;
+        }
+}
